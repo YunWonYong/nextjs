564b1c6e81a9791ce99cae647b751c482440434a
wonyong.yun <wonyong.yun@playlinks.com>
2024-04-21 21:39:03 +0900

    react NewPost component create

--- create-crash-course/src/components/PostList.jsx
+++ create-crash-course/src/components/PostList.jsx
@@ -1,29 +1,33 @@
 import classes from "./PostList.module.css";
+import NewPost from "./NewPost";
+
 import Post from "./Post";
 
 const PostList = () => {
     return (
-        <ul
-            className={ classes.posts }
-        >
-            <Post 
-                author="Maximilian"
-                body="React.js is awesome!"
-            />
-            <Post 
-                author="Manuel"
-                body="Check out the full course!"
-            />
-            {/* <Post 
-                author=""
-                body=""
-            />
-            <Post 
-                author=""
-                body=""
-            /> */}
-        </ul>
-
+        <>
+            <NewPost />
+            <ul
+                className={ classes.posts }
+            >
+                <Post 
+                    author="Maximilian"
+                    body="React.js is awesome!"
+                />
+                <Post 
+                    author="Manuel"
+                    body="Check out the full course!"
+                />
+                {/* <Post 
+                    author=""
+                    body=""
+                />
+                <Post 
+                    author=""
+                    body=""
+                /> */}
+            </ul>
+        </>
     );
 };
 
