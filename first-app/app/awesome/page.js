8794d4daeaedc8c79bbb7f601f592a95d6ad25c0
wonyong.yun <wonyong.yun@playlinks.com>
2024-04-21 19:48:36 +0900

    awesome page

---
+++ first-app/app/awesome/page.js
@@ -0,0 +1,11 @@
+const AwesomePage = () => {
+    return (
+        <main>
+            <h1>
+                NextJs Is Awesom!   
+            </h1>
+        </main>
+    );
+};
+
+export default AwesomePage;
