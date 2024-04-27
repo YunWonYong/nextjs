9c551cb2e90c5172056e7254cf78b2b69543b1db
wonyong.yun <wonyong.yun@playlinks.com>
2024-04-27 20:01:47 +0900

    react PostList component addPostHandler server api update

--- create-crash-course/src/components/PostList.jsx
+++ create-crash-course/src/components/PostList.jsx
@@ -15,6 +15,13 @@ const PostList = ({ isPosting, onStopPosting }) => {
     ]);
     
     const addPostHandler = (postData) =>{
+        fetch("http://localhost:8080/posts", {
+            method: "POST",
+            body: JSON.stringify(postData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
         setPosts((existingPosts) => [postData, ...existingPosts]);
     };
     return (
