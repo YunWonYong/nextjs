9b9379fdba6058ef8dd3fbb098e9c6b899959121
wonyong.yun <wonyong.yun@playlinks.com>
2024-04-27 20:28:03 +0900

    react PostList component side effect loading logic update

--- create-crash-course/src/components/PostList.jsx
+++ create-crash-course/src/components/PostList.jsx
@@ -8,11 +8,17 @@ import Post from "./Post";
 
 const PostList = ({ isPosting, onStopPosting }) => {
     const [ posts, setPosts ] = useState([]);
+    const [ isFetching, setFetching ] = useState(false);
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await fetch("http://localhost:8080/posts");
-            const data = await response.json();
-            setPosts(data.posts);
+            try {
+                setFetching(() => true);
+                const response = await fetch("http://localhost:8080/posts");
+                const data = await response.json();
+                setPosts(data.posts);
+            } finally {
+                setFetching(() => false);
+            }
         };
         fetchPosts();
     }, []);
@@ -44,29 +50,40 @@ const PostList = ({ isPosting, onStopPosting }) => {
                 className={ classes.posts }
             >
                 {
-                    posts.length > 0
-                        ?   posts.map(({ author, body }, index) => {
-                                return (
-                                    <Post
-                                        key={ `${author}_${index}` }
-                                        author={ author }
-                                        body={ body }
-                                    />
-                                );
-                            })
-                        :   <div
+                    isFetching
+                        ?   <div        
                                 style={{
                                     textAlign: "cender",
                                     color: "white"
                                 }}
                             >
-                                <h2>
-                                    There are no posts yet.
-                                </h2>
                                 <p>
-                                    Start adding some!
+                                    Loading posts...
                                 </p>
                             </div>
+                        :   posts.length > 0
+                                ?   posts.map(({ author, body }, index) => {
+                                        return (
+                                            <Post
+                                                key={ `${author}_${index}` }
+                                                author={ author }
+                                                body={ body }
+                                            />
+                                        );
+                                    })
+                                :   <div
+                                        style={{
+                                            textAlign: "cender",
+                                            color: "white"
+                                        }}
+                                    >
+                                        <h2>
+                                            There are no posts yet.
+                                        </h2>
+                                        <p>
+                                            Start adding some!
+                                        </p>
+                                    </div>
                 }
             </ul>
         </>
