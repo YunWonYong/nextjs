27ee92f6fbd683cbe8e10e5418d90277783f1815
wonyong.yun <wonyong.yun@playlinks.com>
2024-10-03 15:52:54 +0900

    add. 4 section Parallel routing

---
+++ 4_routingAndPageRendering/app/archive/@archive/page.js
@@ -0,0 +1,15 @@
+const ArchivePage = () => {
+    return (
+        <header id="archive-header">
+            <nav>
+                <ul>
+                    <li>
+                         
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default ArchivePage;
---
+++ 4_routingAndPageRendering/app/archive/@latest/page.js
@@ -0,0 +1,9 @@
+const LatestPage = () => {
+    return (
+        <h1>
+            Latest Page
+        </h1>
+    );
+};
+
+export default LatestPage;
