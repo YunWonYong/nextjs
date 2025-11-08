8071a1c4a2b53caea2e443f0420931e648bdc734
YunWonYong <ywyi1992@gmail.com>
2025-11-08 23:33:04 +0900

    [second] nextjs에서 페이지를 못찾을 때 처리하는 방법.

---
+++ 2_second/app/not-found.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>
+                Not found
+            </h1>
+            <p>
+                Unfortunately, we cloud not find the requested page or resource.
+            </p>
+        </main>
+    );
+}
