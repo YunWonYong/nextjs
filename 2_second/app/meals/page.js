1b1505567e0e5615a0cfebe14d4e12e7b34de065
YunWonYong <ywyi1992@gmail.com>
2025-11-08 23:23:10 +0900

    [second] 조금 더 세분화된 로딩 기능 만들기. Suspense

--- 2_second/app/meals/page.js
+++ 2_second/app/meals/page.js
@@ -2,14 +2,21 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+    const meals = await getMeals();
+    return (
+        <MealsGrid meals={ meals } />
+    );
+}
 
 export default async function MealsPage() {
-    const meals = await getMeals();
     return (
         <>
             <header className={ classes.header }>
                 <h1>
-                    Delicious meals, created{ '' }
+                    Delicious meals, created{ ' ' }
                     <span className={ classes.highlight }>
                         by you
                     </span>
@@ -24,7 +31,9 @@ export default async function MealsPage() {
                 </p>
             </header>
             <main className={ classes.main }>
-                <MealsGrid meals={ meals } />
+                <Suspense fallback={ <p className={ classes.loading } > Fetching meals...</p> }>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     );
