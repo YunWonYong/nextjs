86b3e13c77ad8d0b6981d5971480adc7d75101c9
YunWonYong <ywyi1992@gmail.com>
2025-11-08 22:31:48 +0900

    [second] usePathname을 사용해서 현재 페이지의 버튼인 경우 css 추가하는 로직 구현.

--- 2_second/components/main-header/index.js
+++ 2_second/components/main-header/index.js
@@ -1,11 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import { MainHeaderBackground } from "./main-header-background";
 
 export default function MainHeader() {
+    const pathname = usePathname();
     return (
         <>
             <MainHeaderBackground />
@@ -21,12 +25,18 @@ export default function MainHeader() {
                 <nav className={ classes.nav }>
                     <ul>
                         <li>
-                            <Link href="/meals">
+                            <Link 
+                                href="/meals"
+                                className={ pathname.startsWith("/meals") ? classes.active: undefined }
+                            >
                                 Browse Meals
                             </Link>
                         </li>
                         <li>
-                            <Link href="/community">
+                            <Link 
+                                href="/community"
+                                className={ pathname.startsWith("/community") ? classes.active: undefined }
+                            >
                                 Foodies Community
                             </Link>
                         </li>
